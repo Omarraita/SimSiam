5859987fe3d2cfd35f22a1b74367d6add86f8ed8
Omarraita <71269935+Omarraita@users.noreply.github.com>
2021-04-18 18:51:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ from tqdm import tqdm
 from model import Model
 from utils import CIFAR10Pair, test_transform, train_transform
 
+from CardioSimCLR import get_cardio_smclr
+from CardioSimCLR_test import get_cardio_smclr_test
+
 
 # train for one epoch to learn features
 def train(net, data_loader, train_optimizer):
     net.train()
     total_loss, total_num, train_bar = 0.0, 0, tqdm(data_loader, dynamic_ncols=True)
-    for pos_1, pos_2, _ in train_bar:
+    for images, _ in train_bar:
+        pos_1 = images[0]
+        pos_2 = images[1]
         pos_1, pos_2 = pos_1.cuda(non_blocking=True), pos_2.cuda(non_blocking=True)
         feature_1, proj_1 = net(pos_1)
         feature_2, proj_2 = net(pos_2)
@@ -38,32 +43,53 @@ def train(net, data_loader, train_optimizer):
 
     return total_loss / total_num
 
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # test for one epoch, use weighted knn to find the most similar images' label to assign the test image
 def test(net, memory_data_loader, test_data_loader):
     net.eval()
     total_top1, total_top5, total_num, feature_bank = 0.0, 0.0, 0, []
     with torch.no_grad():
         # generate feature bank
-        for data, _, _ in tqdm(memory_data_loader, desc='Feature extracting', dynamic_ncols=True):
-            feature, proj = net(data.cuda(non_blocking=True))
+        
+        for data, targets in tqdm(memory_data_loader, desc='Feature extracting', dynamic_ncols=True):
+            #print(len(targets.tolist()))
+            if(len(targets.tolist())<20):
+              targets = targets_
+              data = data_
+              
+            feature, proj = net(data[0].cuda(non_blocking=True))
             feature_bank.append(F.normalize(feature, dim=-1))
+            #store previous elements
+            targets_ = targets
+            data_ = data
+
         # [D, N]
         feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
         # [N]
-        feature_labels = torch.tensor(memory_data_loader.dataset.targets, device=feature_bank.device)
+        
+        feature_labels = torch.tensor(targets, device=feature_bank.device)
+        #print('HEREEE', feature_labels.size())
         # loop test data to predict the label by weighted knn search
+        
         test_bar = tqdm(test_data_loader, dynamic_ncols=True)
-        for data, _, target in test_bar:
+        for data, target in test_bar:
+            
+            data = data[0]
+            #print(data.size())
+            print(target[0])
+            target = target.squeeze(1)
             data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+            #data, target = data.cuda(), target.cuda()
             feature, proj = net(data)
-
+            
             total_num += data.size(0)
             # compute cos similarity between each feature vector and feature bank ---> [B, N]
             sim_matrix = torch.mm(F.normalize(feature, dim=-1), feature_bank)
             # [B, K]
             sim_weight, sim_indices = sim_matrix.topk(k=k, dim=-1)
             # [B, K]
+            #print(feature_labels.size())
+            #print(sim_indices)
             sim_labels = torch.gather(feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
             sim_weight = sim_weight.exp()
 
@@ -75,12 +101,14 @@ def test(net, memory_data_loader, test_data_loader):
             pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, c) * sim_weight.unsqueeze(dim=-1), dim=1)
 
             pred_labels = pred_scores.argsort(dim=-1, descending=True)
-            total_top1 += torch.sum((pred_labels[:, :1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-            total_top5 += torch.sum((pred_labels[:, :5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
-            test_bar.set_description('Test Epoch: [{}/{}] Acc@1:{:.2f}% Acc@5:{:.2f}%'
-                                     .format(epoch, epochs, total_top1 / total_num * 100, total_top5 / total_num * 100))
+
+            #total_top1 += torch.sum((pred_labels[:, :1] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+            #total_top5 += torch.sum((pred_labels[:, :5] == target.unsqueeze(dim=-1)).any(dim=-1).float()).item()
+            #test_bar.set_description('Test Epoch: [{}/{}] Acc@1:{:.2f}% Acc@5:{:.2f}%'
+                                     #.format(epoch, epochs, total_top1 / total_num * 100, total_top5 / total_num * 100))
 
     return total_top1 / total_num * 100, total_top5 / total_num * 100
+    
 
 
 if __name__ == '__main__':
@@ -95,6 +123,7 @@ if __name__ == '__main__':
     feature_dim, k, batch_size, epochs = args.feature_dim, args.k, args.batch_size, args.epochs
 
     # data prepare
+    '''
     train_data = CIFAR10Pair(root='data', train=True, transform=train_transform, download=True)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
     memory_data = CIFAR10Pair(root='data', train=True, transform=test_transform, download=True)
@@ -102,6 +131,15 @@ if __name__ == '__main__':
     test_data = CIFAR10Pair(root='data', train=False, transform=test_transform, download=True)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
 
+    '''
+    train_data = get_cardio_smclr('/content/drive/MyDrive/Robotics project I/Datasets/validation_patches_augRed.npz')
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
+    memory_data = get_cardio_smclr_test('/content/drive/MyDrive/Robotics project I/Datasets/training_patches_augRed.npz')
+    memory_loader = DataLoader(memory_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+    test_data = get_cardio_smclr_test('/content/drive/MyDrive/Robotics project I/Datasets/validation_patches_augRed.npz')
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
+    
+    
     # model setup and optimizer config
     model = Model(feature_dim).cuda()
     flops, params = profile(model, inputs=(torch.randn(1, 3, 32, 32).cuda(),), verbose=False)
@@ -109,8 +147,9 @@ if __name__ == '__main__':
     print('# Model Params: {} FLOPs: {}'.format(params, flops))
     optimizer = SGD(model.parameters(), lr=0.03, momentum=0.9, weight_decay=5e-4)
     lr_scheduler = LambdaLR(optimizer, lr_lambda=lambda i: 0.5 * (math.cos(i * math.pi / epochs) + 1))
-    c = len(memory_data.classes)
-
+    #c = len(memory_data.classes)
+    c = 2
+    
     results = {'train_loss': [], 'test_acc@1': [], 'test_acc@5': []}
     save_name_pre = '{}_{}_{}_{}'.format(feature_dim, k, batch_size, epochs)
     if not os.path.exists('results'):
@@ -121,12 +160,12 @@ if __name__ == '__main__':
         train_loss = train(model, train_loader, optimizer)
         results['train_loss'].append(train_loss)
         lr_scheduler.step()
-        test_acc_1, test_acc_5 = test(model, memory_loader, test_loader)
-        results['test_acc@1'].append(test_acc_1)
-        results['test_acc@5'].append(test_acc_5)
+        #test_acc_1, test_acc_5 = test(model, memory_loader, test_loader)
+        #results['test_acc@1'].append(test_acc_1)
+        #results['test_acc@5'].append(test_acc_5)
         # save statistics
-        data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-        data_frame.to_csv('results/{}_statistics.csv'.format(save_name_pre), index_label='epoch')
-        if test_acc_1 > best_acc:
-            best_acc = test_acc_1
-            torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))
+        #data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
+        #data_frame.to_csv('results/{}_statistics.csv'.format(save_name_pre), index_label='epoch')
+        #if test_acc_1 > best_acc:
+            #best_acc = test_acc_1
+    torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))
